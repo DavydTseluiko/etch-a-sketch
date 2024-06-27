2123fbdf2f554a64f1b0d4d51f00d48df0718600
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-27 12:26:29 -0500

    Make creating grids easier through JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 const divContainer = document.querySelector(".container");
 
-for (let i = 0; i < 272; i++) {
-  const div = document.createElement("div");
-  divContainer.appendChild(div);
+function makeRows(rows, columns) {
+  let amountOfPixels = rows * columns + Math.round((rows + columns) / 2);
+
+  for (let i = 0; i < amountOfPixels; i++) {
+    const div = document.createElement("div");
+    divContainer.appendChild(div);
+  }
 }
+
+function makeColumns(columns) {
+  let createColumnsEveryNElement = `
+  .container > div:nth-child(${columns + 1}n) {
+    width: 100%;
+    height: auto;
+  }
+  `;
+
+  let styleSheet = document.createElement("style");
+  styleSheet.textContent = createColumnsEveryNElement;
+  document.head.appendChild(styleSheet);
+}
+
+function makeGrid(rows, columns) {
+  makeRows(rows, columns);
+  makeColumns(columns);
+}
+
+makeGrid(16, 16);
